a090593ab3e4cb8502fb4898562f46d7327aef0d
rich p <richardferry3@gmail.com>
2022-04-24 23:56:58 -0400

    add test for employee

---
+++ tests/employee.test.js
@@ -0,0 +1,37 @@
+const Employee = require('../lib/Employee')
+
+describe("Employee class", () => {
+    const employee = new Employee("wilbert","128","wilbert@email.com");
+
+    it("creates a object with name, id, and email", () => {
+        expect(employee).toEqual({ name: "wilbert", id: "128", email: "wilbert@email.com" });
+    })
+    describe("getName", () => {
+        it("returns name of employee", () => {
+
+            const name = employee.getName()
+            expect(name).toEqual("wilbert")
+        })
+    })
+    describe("getId", () => {
+        it("returns employee id", () => {
+
+            const id = employee.getId()
+            expect(id).toEqual("128")
+        })
+    })
+    describe("getEmail", () => {
+        it("returns employee email", () => {
+
+            const email = employee.getEmail()
+            expect(email).toEqual("wilbert@email.com")
+        })
+    })
+    describe("getRole", () => {
+        it("returns individual role", () => {
+
+            const role = employee.getRole()
+            expect(role).toEqual("Employee")
+        })
+    })
+})
